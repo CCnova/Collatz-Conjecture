79555c71a1444891fdda4ed303cf3b698c317de3
CCnova <caiobahia.nova@gmail.com>
2018-11-04 06:27:26 -0300

    Added a function to show the sequence

--- CollatzProgram.cpp
+++ CollatzProgram.cpp
@@ -1,7 +1,7 @@
 //Program to implement the Collatz Conjecture
 #include <iostream>
 
-int collatz(int x, int number)
+int collatzSteps(int x, int number)
 {
 
 	if(x <= 1)
@@ -12,13 +12,33 @@ int collatz(int x, int number)
 	else if(x%2 == 0)
 	{
 		++number;
-		collatz(x/2, number);
+		collatzSteps(x/2, number);
 	}
 
 	else
 	{
 		++number;
-		collatz(x*3 + 1, number);
+		collatzSteps(x*3 + 1, number);
+	}
+}
+
+void collatz(int x)
+{
+	if(x <= 1)
+	{
+		return;
+	}
+
+	else if(x%2 == 0)
+	{
+		std::cout << x/2 << " ";
+		collatz(x/2);
+	}
+
+	else
+	{
+		std::cout << x*3 + 1 << " ";
+		collatz(x*3 + 1);
 	}
 }
 
@@ -28,7 +48,12 @@ int main()
 	std::cout << "Enter the number wich we will apply the Collatz Conjecture (n > 1): " << std::endl;
 	std::cin >> n;
 
-	std::cout << "It took " << collatz(n, steps) << " steps to reach one" << std::endl;
+	std::cout << "It took " << collatzSteps(n, steps) << " steps to reach one" << std::endl;
+
+	std::cout << "Here is the sequence: " << std::endl;
+	collatz(n);
+
+	std::cout << '\n';
 
 	return 0;
 }
